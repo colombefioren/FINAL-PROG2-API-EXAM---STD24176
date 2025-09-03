a6c7d25c13c2d06a2174e89c4ec7dfb617dca2c2
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-03 09:04:07 +0300

    fix: add '/' to health endpoint

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def get_pong():
     return Response(content="pong", media_type="text/plain",status_code=200)
 
-@app.get("health")
+@app.get("/health")
 def get_health():
     return Response(content="Ok",media_type="text/plain,status_code=200")
 
