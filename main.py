b9a9d7f648cdd3876b1461f65d767031ebd9bc4b
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-03 09:03:41 +0300

    feat: GET /health

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/ping")
 def get_pong():
     return Response(content="pong", media_type="text/plain",status_code=200)
+
+@app.get("health")
+def get_health():
+    return Response(content="Ok",media_type="text/plain,status_code=200")
+
